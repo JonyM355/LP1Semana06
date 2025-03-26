36b1a5c29dbf088544003067f3cd9e2deb3de2d8
JonyM355 <joaoricardoam05@gmail.com>
2025-03-26 15:59:14 +0000

    Using class Random in a code

---
+++ RandDice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random seed = new Random(int.Parse(args[1]));
+
+            int sum = 0;
+
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Demasiados valores!");
+                return;
+            }
+            else if (args.Length <= 1)
+            {
+                Console.WriteLine("Precisas no mínimo 2 valores!");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < int.Parse(args[0]); i++)
+                {
+                    int rslt = seed.Next(1, 7);
+                    
+                    sum += rslt;
+                }
+
+                Console.WriteLine(sum);
+            }
+            
+        }
+    }
+}
