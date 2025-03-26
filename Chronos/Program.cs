ba191286dbc8969228bffd1db37f7b1667ab92e3
JonyM355 <joaoricardoam05@gmail.com>
2025-03-26 15:27:01 +0000

    Using class Stopwatch

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,6 +10,17 @@ namespace Chronos
         {
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500);
+            crono1.Stop();
+
+            crono2.Start();
+            Thread.Sleep(750);
+            crono2.Stop();
+
+            Console.WriteLine($"Crono1: {crono1.Elapsed.TotalSeconds:f4}s");
+            Console.WriteLine($"Crono2: {crono2.Elapsed.TotalSeconds:f4}s");
         }
     }
 }
