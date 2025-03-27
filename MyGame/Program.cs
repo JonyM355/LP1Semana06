49682b60f116fb75debe8a304b7d7cb0aeda3fd4
JonyM355 <joaoricardoam05@gmail.com>
2025-03-27 09:59:29 +0000

    Add class Enemy

---
+++ MyGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+
+    public class Enemy
+    {
+
+    }
+}
