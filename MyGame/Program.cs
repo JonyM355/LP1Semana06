a1584e637d7a5280500d76b1bf3ad699bd2ac60d
JonyM355 <joaoricardoam05@gmail.com>
2025-03-27 15:41:00 +0000

    Trying to do Ex.10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters;
 
 namespace MyGame
 {
@@ -7,7 +10,65 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Insert 1 number!");
+                return;
+            }
+
+           
+            string argumento = args[0];
+
+
+            bool isNumber = true;
+            foreach (char c in argumento)
+            {
+                if (!char.IsDigit(c))
+                {
+                    isNumber = false;
+                    break;
+                }
+            }
+
+
+            if (!isNumber)
+            {
+                Console.WriteLine("Insert a number");
+                return;
+            }
+
+
+            int numEnemies = int.Parse(argumento);
+
+            if (numEnemies <= 0)
+            {
+                Console.WriteLine("Number of enemies should be higher than 0");
+                return;
+            }
+
+            if (args.Length != numEnemies +1)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+
+            Enemy[] enmList = new Enemy[numEnemies];
+
+            for (int i = 0; i < numEnemies; i++)
+            {
+                string nome = args[i + 1];
+                enmList[i] = new Enemy(nome);
+            }
+            
+
+            Console.WriteLine("Informações dos inimigos:");
+            for (int i = 0; i < enmList.Length; i++)
+            {
+                Console.WriteLine("Nome: " + enmList[i].GetName() + 
+                                ", Health: " + enmList[i].GetHealth() +
+                                ", Shield " + enmList[i].GetShield());
+            }
         }
     }
 
